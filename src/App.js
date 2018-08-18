40f69dc065086452157a8e53c9714ce8995c60e0
Jared <kuil5230@gmail.com>
2018-08-18 15:11:49 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,20 @@
 import React, { Component } from "react";
 
-import store from "./Store";
 import * as Action from "./Action";
+import { connect } from "react-redux";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: store.getState()
-    };
-  }
-  onIncrement = () => {
-    store.dispatch(Action.increment());
-  };
-
-  onDecrement = () => {
-    store.dispatch(Action.decrement());
-  };
-
   render() {
-    store.subscribe(() =>
-      this.setState({
-        count: store.getState()
-      })
-    );
+    const { increment, decrement } = this.props;
+
     return (
       <div className="container">
-        <h1 className="text-center mt-5">{this.state.count}</h1>
+        <h1 className="text-center mt-5">{this.props.count}</h1>
         <p className="text-center">
-          <button onClick={this.onIncrement} className="btn btn-primary mr-2">
+          <button onClick={() => increment()} className="btn btn-primary mr-2">
             Increase
           </button>
-          <button onClick={this.onDecrement} className="btn btn-primary mr-2">
+          <button onClick={() => decrement()} className="btn btn-primary mr-2">
             Decrement
           </button>
         </p>
@@ -40,4 +23,11 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  count: state
+});
+
+export default connect(
+  mapStateToProps,
+  Action
+)(App);
